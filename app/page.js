2f1cf7dd3eb4eb85a0238eac546e8533a385cb70
Ahmed Sayed <ahmed.sayed@devteam.bg>
2026-03-01 04:05:26 +0200

    d

--- app/page.js
+++ app/page.js
@@ -22,14 +22,18 @@ export default function Home() {
       {/* Floating Hotline Button */}
       <a
         href="tel:15596"
-        className="fixed bottom-6 left-6 z-50 block shadow-[0_8px_25px_rgba(0,0,0,0.3)] hover:shadow-[0_12px_35px_rgba(0,0,0,0.4)] hover:scale-105 active:scale-95 transition-all duration-300 cursor-pointer rounded-full overflow-hidden"
+        className="fixed bottom-6 left-6 z-50 flex items-center gap-2 shadow-[0_8px_25px_rgba(0,0,0,0.3)] hover:shadow-[0_12px_35px_rgba(0,0,0,0.4)] hover:scale-105 active:scale-95 transition-all duration-300 cursor-pointer rounded-full overflow-hidden px-3 py-2"
+        style={{ background: '#00a7a9' }}
         aria-label="اتصل بالخط الساخن 15596"
       >
         <img 
           src="/images/hotline.png" 
           alt="15596"
-          className="w-auto h-16 object-contain rounded-full"
+          className="w-auto h-12 object-contain"
         />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-6 h-6 flex-shrink-0">
+          <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+        </svg>
       </a>
     </main>
   )
